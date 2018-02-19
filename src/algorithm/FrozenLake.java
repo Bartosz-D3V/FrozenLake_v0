54c81e5559e18fd3fc06b243493751d6836ae346
Bartosz-D3V <nix.noxi379@gmail.com>
2018-02-19 07:54:07 +0000

    feat: added constants file and isFinalState method

--- src/algorithm/FrozenLake.java
+++ src/algorithm/FrozenLake.java
@@ -1,4 +1,25 @@
 package algorithm;
 
-public class FrozenLake {
+import common.Constants;
+
+import java.io.Serializable;
+
+public class FrozenLake implements Serializable {
+  private final char[][] lake;
+  private final char[][] rewardLookup = new char[Constants.states][Constants.states];
+
+  public FrozenLake(final char[][] lake) {
+    this.lake = lake;
+  }
+
+  private void calcQ() {
+
+  }
+
+  private boolean isFinalState(final int index) {
+    final int i = index / Constants.mazeWidth;
+    final int j = index / Constants.mazeHeight;
+
+    return lake[i][j] == Constants.endStep;
+  }
 }
--- src/domain/Lake.java
+++ src/domain/Lake.java
@@ -1,7 +1,43 @@
 package domain;
 
+import common.Constants;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 public class Lake implements Serializable {
-    private char[][] lake;
+  private char[][] lake;
+
+  public Lake() {
+    super();
+  }
+
+  public final char[][] getLake() throws IOException {
+    lake = new char[Constants.mazeHeight][Constants.mazeWidth];
+    final File file = new File("resources/frozen-lake.txt");
+    try (final FileInputStream fileInputStream = new FileInputStream(file)) {
+      int i = 0;
+      int j = 0;
+      int content;
+      while ((content = fileInputStream.read()) != -1) {
+        final char c = (char) content;
+        if (c == Constants.endStep) {
+          break;
+        }
+        lake[i][j] = c;
+        ++j;
+        if (j == Constants.mazeHeight) {
+          j = 0;
+          ++i;
+        }
+      }
+      fileInputStream.close();
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+
+    return lake;
+  }
 }
