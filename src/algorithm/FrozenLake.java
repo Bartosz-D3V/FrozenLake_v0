bafc41dd0480f862b448b6e128e8af90136caa21
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2018-02-21 09:10:49 +0000

    methods to calc maxQ and possible movements (#4)

--- src/algorithm/FrozenLake.java
+++ src/algorithm/FrozenLake.java
@@ -9,8 +9,8 @@ import java.util.Random;
 
 public final class FrozenLake implements Serializable {
   private final char[][] lake;
-  private final char[][] rewardLookup = new char[Constants.STATES][Constants.STATES];
-  private final char[][] q = new char[Constants.STATES][Constants.STATES];
+  private final double[][] rewardLookup = new double[Constants.STATES][Constants.STATES];
+  private final double[][] q = new double[Constants.STATES][Constants.STATES];
 
   public FrozenLake(final char[][] lake) {
     this.lake = lake;
@@ -19,7 +19,7 @@ public final class FrozenLake implements Serializable {
   private void calcQ() {
     final Random random = new Random();
     for (int i = 0; i < Settings.CYCLES; ++i) {
-      final int initialState = random.nextInt(Constants.STATES);
+      int currentState = random.nextInt(Constants.STATES);
 
       while (!isFinalState(i)) {
         // Retrieve all available actions from current state
@@ -35,9 +35,47 @@ public final class FrozenLake implements Serializable {
           Q(state, action) = Q(state, action) + alpha * (R(state, action) +
           gamma * Max(next state, all possible actions) - Q(state, action))
          */
-        final double tempQ = q[initialState][newRandomAction];
+        final double tempQ = q[currentState][newRandomAction];
+        final double tempMaxQ = calcMaxQ(nextState);
+        final double tempR = rewardLookup[currentState][nextState];
+        final double formulaResult = tempQ + Settings.ALPHA + tempR +
+          Settings.GAMMA * (tempR + Settings.GAMMA * tempMaxQ - tempQ);
+        q[currentState][nextState] = formulaResult;
+
+        currentState = nextState;
       }
     }
+  }
+
+  private double calcMaxQ(final int nextState) {
+    final int[] possibleActionsFromState = getPossibleActionsFromState(nextState);
+    // maxVal initially set to the lowest possible value
+    double maxVal = Double.MIN_VALUE;
+    for (int possibleNextAction : possibleActionsFromState) {
+      final double currentValue = q[nextState][possibleNextAction];
+
+      if (currentValue > maxVal) {
+        maxVal = currentValue;
+      }
+    }
+    return maxVal;
+  }
+
+  private int getNextMovement(final int currentState) {
+    final int[] actionsFromState = getPossibleActionsFromState(currentState);
+    int nextMovement = currentState;
+    // maxValue initially set to the lowest possible value
+    double maxValue = Double.MIN_VALUE;
+
+    for (int nextPossibleMovement : actionsFromState) {
+      final double movementValue = q[currentState][nextMovement];
+      if (movementValue > maxValue) {
+        maxValue = movementValue;
+        nextMovement = nextPossibleMovement;
+      }
+    }
+
+    return nextMovement;
   }
 
   private int[] getPossibleActionsFromState(final int state) {
