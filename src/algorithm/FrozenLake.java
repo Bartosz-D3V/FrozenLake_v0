8b35b52c0497e8f4a78fea016d2ae2b1b81ebb24
Bartosz-D3V <nix.noxi379@gmail.com>
2018-04-04 07:40:31 +0100

    feat: extended lake, added JavaDoc

--- src/algorithm/FrozenLake.java
+++ src/algorithm/FrozenLake.java
@@ -12,38 +12,48 @@ public final class FrozenLake implements Serializable {
   private final double[][] rewardLookup = new double[Constants.STATES][Constants.STATES];
   private final double[][] q = new double[Constants.STATES][Constants.STATES];
 
+  /**
+   * Construct FrozenLake class based on
+   * lake of type char[][]
+   *
+   * @param lake lake
+   */
   public FrozenLake(final char[][] lake) {
     this.lake = lake;
   }
 
+  /**
+   * Populate reward lookup table (R) with
+   * values calculated based on Q learning algorithm
+   * formula
+   */
   public void populateRewardLookupTable() {
     for (int k = 0; k < Constants.STATES; k++) {
       final int i = k / Constants.MAZE_WIDTH;
       final int j = k - i * Constants.MAZE_WIDTH;
 
-      // Initially, we set each element in lake with -1
-      for (int s = 0; s < Constants.STATES; s++) {
-        rewardLookup[k][s] = -1;
-      }
-
-      // Try to move in all directions if not a final state
+      // Try to move in all directions if not in a final state
       if (lake[i][j] != Constants.END_STEP) {
 
+        // Move left from current position in frozen lake
         final int moveLeft = j - 1;
         if (moveLeft > -1) {
           moveHorizontally(k, i, moveLeft);
         }
 
+        // Move right from current position in frozen lake
         final int moveRight = j + 1;
         if (moveRight < Constants.MAZE_WIDTH) {
           moveHorizontally(k, i, moveRight);
         }
 
+        // Move up from current position in frozen lake
         final int moveUp = i - 1;
         if (moveUp > -1) {
           moveVertically(k, j, moveUp);
         }
 
+        // Move down from current position in frozen lake
         final int moveDown = i + 1;
         if (moveDown < Constants.MAZE_HEIGHT) {
           moveVertically(k, j, moveDown);
@@ -52,6 +62,9 @@ public final class FrozenLake implements Serializable {
     }
   }
 
+  /**
+   * Populate Q array with calculated Q values
+   */
   public void calcQ() {
     final Random random = new Random();
     for (int i = 0; i < Settings.CYCLES; i++) {
@@ -83,6 +96,13 @@ public final class FrozenLake implements Serializable {
     }
   }
 
+  /**
+   * Move either left or right in a reward lookup table
+   *
+   * @param k              k
+   * @param i              i
+   * @param horizontalMove horizontalMove
+   */
   private void moveHorizontally(final int k, final int i, final int horizontalMove) {
     final int target = i * Constants.MAZE_WIDTH + horizontalMove;
     if (lake[i][horizontalMove] == 'O') {
@@ -94,6 +114,13 @@ public final class FrozenLake implements Serializable {
     }
   }
 
+  /**
+   * Move either up or down in a reward lookup table
+   *
+   * @param k            k
+   * @param j            j
+   * @param verticalMove verticalMove
+   */
   private void moveVertically(final int k, final int j, final int verticalMove) {
     final int target = verticalMove * Constants.MAZE_WIDTH + j;
     if (lake[verticalMove][j] == 'O') {
@@ -105,11 +132,17 @@ public final class FrozenLake implements Serializable {
     }
   }
 
+  /**
+   * Return maximum Q values for all available actions from a given state
+   *
+   * @param nextState nextState
+   * @return int nextState
+   */
   private double calcMaxQ(final int nextState) {
     final int[] possibleActionsFromState = getPossibleActionsFromState(nextState);
     // maxVal initially set to the lowest possible value
     double maxVal = Double.MIN_VALUE;
-    for (int possibleNextAction : possibleActionsFromState) {
+    for (final int possibleNextAction : possibleActionsFromState) {
       final double currentValue = q[nextState][possibleNextAction];
 
       if (currentValue > maxVal) {
@@ -119,13 +152,19 @@ public final class FrozenLake implements Serializable {
     return maxVal;
   }
 
+  /**
+   * Select best action from a given state
+   *
+   * @param currentState currentState
+   * @return int currentState
+   */
   private int getNextMovement(final int currentState) {
     final int[] actionsFromState = getPossibleActionsFromState(currentState);
     int nextMovement = currentState;
     // maxValue initially set to the lowest possible value
     double maxValue = Double.MIN_VALUE;
 
-    for (int nextPossibleState : actionsFromState) {
+    for (final int nextPossibleState : actionsFromState) {
       final double movementValue = q[currentState][nextPossibleState];
       if (movementValue > maxValue) {
         maxValue = movementValue;
@@ -136,6 +175,12 @@ public final class FrozenLake implements Serializable {
     return nextMovement;
   }
 
+  /**
+   * Get possible states from a given state
+   *
+   * @param state state
+   * @return int[] possibleActions
+   */
   private int[] getPossibleActionsFromState(final int state) {
     final List<Integer> possibleActions = new ArrayList<>();
     for (int i = 0; i < Constants.STATES; i++) {
@@ -146,6 +191,12 @@ public final class FrozenLake implements Serializable {
     return possibleActions.stream().mapToInt(i -> i).toArray();
   }
 
+  /**
+   * Checks whether given index is a final step
+   *
+   * @param index index
+   * @return boolean isFinalState
+   */
   private boolean isFinalState(final int index) {
     final int i = index / Constants.MAZE_WIDTH;
     final int j = index - i * Constants.MAZE_WIDTH;
@@ -153,6 +204,9 @@ public final class FrozenLake implements Serializable {
     return lake[i][j] == Constants.END_STEP;
   }
 
+  /**
+   * Print all states
+   */
   public void printSelectedStates() {
     for (int state = 0; state < Constants.STATES; state++) {
       final int nextState = getNextMovement(state);
--- src/common/Constants.java
+++ src/common/Constants.java
@@ -3,8 +3,8 @@ package common;
 import java.io.Serializable;
 
 public final class Constants implements Serializable {
-  public static final short MAZE_HEIGHT = 3;
-  public static final short MAZE_WIDTH = 4;
+  public static final short MAZE_HEIGHT = 6;
+  public static final short MAZE_WIDTH = 8;
   public static final short STATES = MAZE_HEIGHT * MAZE_WIDTH;
   public static final char START_STEP = 'S';
   public static final char END_STEP = 'E';
