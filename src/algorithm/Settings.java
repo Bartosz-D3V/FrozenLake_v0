ad3095b716303bfc271b1cfea63f681b95cc9691
Bartosz-D3V <nix.noxi379@gmail.com>
2018-04-07 19:42:17 +0100

    feat: added method to print R

--- src/algorithm/Settings.java
+++ src/algorithm/Settings.java
@@ -6,6 +6,6 @@ public final class Settings implements Serializable {
   public static final int CYCLES = 1000;
   public static final double ALPHA = 0.1;
   public static final double GAMMA = 0.9;
-  public static final int REWARD = 1000;
+  public static final int REWARD = 2000;
   public static final int PENALTY = -1000;
 }
