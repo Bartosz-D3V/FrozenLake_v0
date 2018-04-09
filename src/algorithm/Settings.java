193c5a4f461bb9e1a3c72c42860984bb7f585abe
Bartosz-D3V <nix.noxi379@gmail.com>
2018-04-09 21:05:25 +0100

    changed learning rate to 0.9

--- src/algorithm/Settings.java
+++ src/algorithm/Settings.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public final class Settings implements Serializable {
   public static final int CYCLES = 1000;
-  public static final double ALPHA = 0.1;
+  public static final double ALPHA = 0.9;
   public static final double GAMMA = 0.9;
   public static final int REWARD = 2000;
   public static final int PENALTY = -1000;
